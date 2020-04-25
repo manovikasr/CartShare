97eaa0b76facdfa9b92e9acd1f59a98a0081ffaa
RitwikJadhav <55038380+RitwikJadhav@users.noreply.github.com>
2020-04-25 14:38:06 -0700

    basic email service added

---
+++ backend/src/main/java/com/cmpe275project/service/EmailServiceImpl.java
@@ -0,0 +1,54 @@
+package com.cmpe275project.service;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import freemarker.core.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
+
+public class EmailServiceImpl implements EmailService{
+
+	@Autowired
+	private JavaMailSender emailSender;
+	
+	@Autowired
+	private Configuration freeMarkerConfig;
+	
+	@Override
+	public void sendEmail(String to, String subject, Map<String, Object> map) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+		// TODO Auto-generated method stub
+		try {
+			MimeMessage message = emailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+			
+			freeMarkerConfig.setClassForTemplateLoading(this.getClass(), "/");
+			
+			Template t = freeMarkerConfig.getTemplate("SimpleEmail.ftl");
+			String text = FreeMarkerTemplateUtils.processTemplateIntoString(t, map);
+			
+			helper.setTo(to);
+			helper.setSubject(subject);
+			helper.setText(text, true);
+			
+			emailSender.send(message);
+			
+		} catch (MessagingException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ backend/src/main/resources/SimpleEmail.ftl
@@ -0,0 +1,8 @@
+<html>
+<head><head>
+
+<body>
+	<p>Dear ${name},</p>
+	<p>Thank you for using CartShare</p>	
+</body>
+</html>
