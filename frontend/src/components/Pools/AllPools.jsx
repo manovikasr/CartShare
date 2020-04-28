8962700d9379b19d459940a60b91fbc1c614cbd7
Manovikas Ramaswamy <manovikas.ramaswamy@sjsu.edu>
2020-04-27 21:05:24 -0700

    Messages Frontend

--- frontend/src/components/Pools/AllPools.jsx
+++ frontend/src/components/Pools/AllPools.jsx
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Alert, Card, Button, Row, Col } from "react-bootstrap";
+import PoolCard from "./PoolCard";
 
 class AllPools extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            pools: []
+        };
     }
 
     componentDidMount() {
-        if (!this.props.auth.isAuthenticated && this.props.auth.user.role === "admin" ) {
+        if (!this.props.auth.isAuthenticated && this.props.auth.user.role === "admin") {
             this.props.history.push("/");
         }
         else {
@@ -22,10 +26,22 @@ class AllPools extends Component {
 
     render() {
         const { user } = this.props.auth;
+        var poolData;
+
+        if (this.state.pools.length) {
+            poolData = this.state.pools.map(pool => {
+                return (<PoolCard pool={pool} />);
+            });
+        } else {
+            poolData = (
+                <Alert variant="warning">There are no pools in the system.</Alert>
+            )
+        }
 
         return (
-            <div>
-                <b>All Pools</b>
+            <div style={{ height: "75vh" }} className="container valign-wrapper">
+                <br />
+                {poolData}
             </div>
         );
     }
