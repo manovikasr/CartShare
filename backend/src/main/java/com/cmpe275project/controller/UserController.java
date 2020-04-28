69022fe2f40d01afc20a37652bfd8a19c539fae5
RitwikJadhav <55038380+RitwikJadhav@users.noreply.github.com>
2020-04-27 19:21:49 -0700

    Added message emails

--- backend/src/main/java/com/cmpe275project/controller/UserController.java
+++ backend/src/main/java/com/cmpe275project/controller/UserController.java
@@ -1,10 +1,26 @@
 package com.cmpe275project.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cmpe275project.model.User;
+import com.cmpe275project.responseObjects.UserResponse;
+import com.cmpe275project.service.EmailService;
 import com.cmpe275project.service.UserService;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
 
 @RestController
 public class UserController {
@@ -12,9 +28,36 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	@RequestMapping("/user")
 	public String index()
 	{
 		return "Cart Share";
+	}
+	
+	@PostMapping("/message")
+	public ResponseEntity<?> sendMessage(@RequestParam String from, @RequestParam String to, @RequestParam String text) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+		HttpStatus status;
+		UserResponse response = new UserResponse();
+		if(userService.isEmailExists(from) && userService.isEmailExists(to)) {
+			User user1 = userService.getUserInfoByEmail(from);
+			User user2 = userService.getUserInfoByEmail(to);
+			String user1ScreenName = user1.getScreen_name();
+			Map<String, Object> map = new HashMap<>();
+			map.put("from_screen_name", user1ScreenName);
+			map.put("to_screen_name", user2.getScreen_name());
+			map.put("text", text);
+			
+			emailService.sendMessageEmail(user1ScreenName, to, map);
+			status = HttpStatus.OK;
+			response.setMessage("Message sent succesfully");
+		}
+		else {
+			status = HttpStatus.NOT_FOUND;
+			response.setMessage("User not found");
+		}
+		return new ResponseEntity<>(response, status);
 	}
 }
---
+++ backend/src/main/resources/MessageEmail.ftl
@@ -0,0 +1,12 @@
+<html>
+<head><head>
+
+<body>
+	<p>Hi ${to_screen_name},</p>
+	<br>
+	<p>${text}.</p>
+	<br>
+	<p>Thanks,</p>
+	<p>Cart Share Team</p>	
+</body>
+</html>
